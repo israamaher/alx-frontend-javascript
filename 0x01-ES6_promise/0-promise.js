130bd2a20e81e8555e3f6e69512344e886a5efff
israa maher <you@example.com>
2024-06-27 00:08:34 +0000

    update

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,6 +1,3 @@
 export default function getResponseFromAPI(){
-  return new Promise((resolve, reject) => {
-    if (apiEndpoint) resolve({ status: 200, body: 'Success' });
-    reject(Error('Failed to fetch API'));
-  });
+  return new Promise(() => {});
 }
