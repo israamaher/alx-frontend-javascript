fa0fa1d1157e9cce4f90ec9b47cd9105b9c891b1
israa maher <you@example.com>
2024-06-28 23:41:25 +0000

    update

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -1,6 +1,6 @@
-import ClassRoom from './0-classroom.js';
+import ClassRoom from './0-classroom';
 
-export default initializeRooms(){
- const Room =[new classRoom(19), new classRoom(20), new classRoom(34)];
- return Room;
+export default function initializeRooms() {
+  const rooms = [new ClassRoom(19), new ClassRoom(20), new ClassRoom(34)];
+  return rooms;
 }
---
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -0,0 +1,36 @@
+export class HolbertonCourse{
+ constructor(name, length, students){
+  if(typeof name !== 'string' || typeof length !== 'number' || !Array.isArray(students)){
+   throw new Error('Invalid attribute type');
+  }
+  
+ this._name = name;
+ this._length = length;
+ this._students = students;
+ }
+ 
+ get name(){
+  return this._name;
+ }
+ set name(value){
+  this._name = value;
+ }
+
+ get length(){
+  return this._length;
+ }
+ set length(value){
+  this._length = value;
+ }
+
+ get students() {
+    return this._students;
+  }
+
+  set students(value) {
+    if (!Array.isArray(value)) {
+      throw new Error('Students must be an array');
+    }
+    this._students = value;
+  }
+}
